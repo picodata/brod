f669d438b61bc7f80d531452ccdfd6c04ac94e16
Dmitriy Koltsov <dkoltsov@picodata.io>
2020-07-07 11:41:26 +0300

    feat(brodrust): add bindings to tarantool API

--- brod/brodrust/src/lib.rs
+++ brod/brodrust/src/lib.rs
@@ -1,4 +1,17 @@
+include!("bindings/module.rs");
+
 #[no_mangle]
 pub extern "C" fn hello() {
     println!("Hello there, General Kenobi!");
+}
+
+#[no_mangle]
+pub extern fn rustproc(fiber_id: u64) {
+    for _ in 0..5 {
+        println!("rust fiber {}: before sleep", fiber_id);
+        unsafe {
+            fiber_sleep(0.1);
+        }
+        println!("rust fiber {}: after sleep", fiber_id);
+    }
 }
